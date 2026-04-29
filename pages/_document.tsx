c44185fa0c159190e1d9463feae994ad9180f47b
Anvarjon <201946044@hs.ac.kr>
2026-04-30 03:39:12 +0900

    feat: MUI customized integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,20 @@ export default function Document() {
   console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        {/** SEO => Seatch Engine Optimization **/}
+
+        <meta name="keyword" content={"nestar , nestart.uz, mern, mern nestjs fullstack"}/>
+        <meta name="description" content={
+          "Buy and sell properties anywhere anytime in South Korea. |" +
+          "대한진국 언제 어티서나 부동산을 사고 팔 수 있다."
+        } />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
