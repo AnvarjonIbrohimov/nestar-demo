9d4e19fc4084c95918681ad81c31cd769dcbea72
Anvarjon <201946044@hs.ac.kr>
2026-05-03 05:10:18 +0900

    fix: refactor pags components

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import { NextPage } from "next";
 import { Stack } from "@mui/material";
 
-const Community: NextPage = () => {
+const AgentList: NextPage = () => {
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">AGENT LIST</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(AgentList);
