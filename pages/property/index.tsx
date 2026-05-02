d557e556bd8d753cdc4add7c474165289a9f78dc
Anvarjon <201946044@hs.ac.kr>
2026-05-03 00:43:34 +0900

    feat: publish basic layout components

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -2,10 +2,10 @@ import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
-const PropertList: NextPage = () => {
+const PropertyList: NextPage = () => {
   return (<>
   <Container>PROPERTY LIST</Container>
   </>)
 };
 
-export default withLayoutBasic(PropertList);
+export default withLayoutBasic(PropertyList);
